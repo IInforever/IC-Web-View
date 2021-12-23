d6bc705602839ca363c1261531e9eb90ccc900db
I-Info <49438478+I-Info@users.noreply.github.com>
2021-12-23 23:20:50 +0800

    finish login page & admin login

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>&copy; {{currentYear}}
+    <a :href="COM_MAIN_PAGE" style="line-height: 20px;vertical-align: center" target="_blank">IInforever</a></p>
+</template>
+
+<script setup>
+import {COM_MAIN_PAGE} from "../config/config";
+const currentYear =  new Date().getFullYear()
+</script>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+p {
+  font-size: var(--el-font-size-base);
+  font-weight: var(--el-font-weight-primary);
+}
+a:visited {
+  color: unset;
+}
+</style>
